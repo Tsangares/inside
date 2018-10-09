f5401c83804aa5980427c70f1b54a3b97dd90101
William Wyatt <tsangares@gmail.com>
2018-10-08 23:59:59 -0700

    Update util.py

--- util.py
+++ util.py
@@ -7,11 +7,7 @@ class Point:
     def get():
         return (self.x,self.y)
     
-class Vector:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-        
+class Vector(Point):
     def norm(self):
         return sqrt(self.x**2 + self.y**2)
 
